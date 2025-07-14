65aa132da3419c6d7658b4d2c89758af7afa5d6a
vaja-coder <vaja.sukhishvilite04@geolab.edu.ge>
2025-07-14 22:06:33 +0400

    final form

--- app/__init__.py
+++
@@ -1,2 +0,0 @@
-from configurs import app
-from routs import index,see_info,add_items,edit_items,delete,register,login,logout,buy,ban_user
--- main.py
+++
@@ -1,6 +0,0 @@
-from configurs import app
-app = app
-
-if __name__ == '__main__':
-    from routs import *
-    app.run(debug=True)
